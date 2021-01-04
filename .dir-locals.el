53c98214263f5f847fdc48b26456a1141dbced59
gene <gene@entropyandsons.com>
2021-01-04 17:02:19 -0500

    .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((nil . ((eval git-auto-commit-mode 1)))
- (nil . ((gac-automatically-push-p 't)))
- (nil . ((gac-automatically-add-new-files-p 't))))
+((nil . ((eval git-auto-commit-mode 1)
+         (gac-automatically-push-p 't)
+         (gac-automatically-add-new-files-p 't))))
