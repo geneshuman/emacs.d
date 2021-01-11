50b7d42d03d89ba6288d800440f086bf1c70a0f1
Gene FuckYou <geneshuman@gmail.com>
2021-01-11 10:51:04 -0500

    ../.emacs.d/init.el

--- init.el
+++ init.el
@@ -783,7 +783,7 @@ be found in docstring of `posframe-show'."
   "Build epimorphism & run it."
 
   (let ((epi-exec-ret (selected-window))
-        (cmd (concat "cd " path " && make -j" (number-to-string cores) "-C build && ./epimorphism " args)))
+        (cmd (concat "cd " path " && make -j" (number-to-string cores) " -C build && ./epimorphism " args)))
 
     (setq epi-args args)
     (shelly-times)
