e0192488675ab4b8da283fd5040d5a7729a4fe50
Gene <gene@entropyandsons.com>
2021-06-28 14:08:51 -0400

    resolved merge

--- init.el
+++ init.el
@@ -158,7 +158,6 @@
 
 (require 'vterm)
 (setq vterm-max-scrollback 50000)
-(customize-set-variable 'vterm-keymap-exceptions '("C-c" "C-x" "C-u" "C-g" "C-h" "C-l" "M-x" "M-o" "C-v" "M-v" "C-y" "M-y" "C-M-j" "C-M-i" "C-M-l" "C-M-k" "C-M-g"))
 
 ;;(require 'xterm-color)
 
