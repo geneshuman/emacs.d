b232f2d6c1386762d61176e8ce370d1a7af1aeee
Gene <geneshuman@gmail.com>
2022-08-24 15:33:01 -0400

    init_lsp_min.el

---
+++ init_lsp_min.el
@@ -0,0 +1,18 @@
+(ensure-package-installed 'lsp-mode)
+
+;; c & c++
+(setq c-default-style "linux")
+(add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++17")))
+;;(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+(add-hook 'c-mode-hook
+      '(lambda()
+        (setq c-basic-offset 2)
+        (setq indent-tabs-mode t)))
+
+(setq lsp-keymap-prefix "C-'")
+
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+
+(setq lsp-idle-delay 0.1)  ;; clangd is fast
