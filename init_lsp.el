e5dd3e4f9e54e17136299edf614907966cb81154
Gene <geneshuman@gmail.com>
2022-08-24 14:33:49 -0400

    init_lsp.el

--- init_lsp.el
+++ init_lsp.el
@@ -1,1 +1,79 @@
 (ensure-package-installed 'lsp-mode 'lsp-ui 'lsp-ivy 'lsp-treemacs 'ccls 'dap-mode 'modern-cpp-font-lock 'cmake-mode)
+
+;; c & c++
+(setq c-default-style "linux")
+(add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++17")))
+;;(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+(add-hook 'c-mode-hook
+      '(lambda()
+        (setq c-basic-offset 2)
+        (setq indent-tabs-mode t)))
+
+(setq lsp-keymap-prefix "C-'")
+
+
+(use-package lsp-mode :commands lsp :ensure t)
+(use-package lsp-mode
+    :hook (((c-mode c++-mode objc-mode cuda-mode) . lsp)
+           (lsp-mode . lsp-enable-which-key-integration))
+    :custom
+    ;;(lsp-auto-guess-root t)
+    (lsp-prefer-capf t)
+    :commands lsp)
+
+(setq lsp-log-io nil)
+
+(setq lsp-enable-on-type-formatting nil)
+(setq lsp-enable-indentation nil)
+(setq lsp-enable-symbol-highlighting nil)
+(setq lsp-before-save-edits nil)
+
+(setq lsp-idle-delay 0.1)
+
+(add-hook 'lsp-ui-mode-hook
+          (lambda()
+            (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+            (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+;;            (lsp-register-client
+;;             (make-lsp-client :new-connection (lsp-tramp-connection "ccls")
+;;                              :major-modes '(c++-mode)
+;;                              :remote? t
+;;                              :server-id 'ccls-remote))
+            ))
+
+
+(with-eval-after-load 'lsp-mode
+  ;; :project/:workspace/:file
+  (setq lsp-diagnostics-modeline-scope :project)
+  (add-hook 'lsp-managed-mode-hook 'lsp-diagnostics-modeline-mode))
+
+;;(lsp-treemacs-sync-mode 1)
+
+(use-package lsp-ui :commands lsp-ui-mode :ensure t)
+(setq lsp-ui-sideline-delay 0.0)
+(setq lsp-ui-sideline-show-code-actions nil)
+(setq lsp-ui-doc-position 'bottom)
+(setq lsp-ui-doc-alignment 'window)
+;;(setq lsp-ui-doc-use-webkit t)
+(setq lsp-ui-doc-enable nil)
+
+(global-set-key (kbd "C-M-d") 'lsp-ui-doc-glance)
+
+
+;;(use-package ccls
+;;  :hook ((c-mode c++-mode objc-mode cuda-mode) .
+;;         (lambda () (require 'ccls) (lsp)))
+;;  :config
+;;  (setq ccls-initialization-options '(:compilationDatabaseDirectory "build")))
+;;
+;;  ;;(setq ccls-initialization-options '(:cache (:directory ".ccls-cache2"))))
+;;  ;;(setq ccls-initialization-options '(:index (:initialBlacklist ["extern"]))))
+
+(require 'lsp-mode)
+(require 'lsp-ui)
+(require 'lsp-ui-doc)
+(add-hook 'c++-mode-hook 'lsp)
+
+;;(dap-auto-configure-mode 1)
+;;(require 'dap-lldb)
