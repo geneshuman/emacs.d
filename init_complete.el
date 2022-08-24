713bc554a72f776396314de0521ea664ad925e06
Gene <geneshuman@gmail.com>
2022-08-24 14:24:56 -0400

    init_complete.el

--- init_complete.el
+++ init_complete.el
@@ -1,1 +1,3 @@
 (ensure-package-installed 'auto-package-update 'ivy 'avy  'flx 'ivy-rich 'ivy-hydra 'ivy-posframe 'ivy-xref 'counsel 'counsel-projectile 'company 'company-box 'company-c-headers)
+
+(require 'flx)
