f9256df5500fce7317231f125939b6d195ab8fa0
Gene Shuman <gene@entropyandsons.com>
2022-12-23 10:30:12 -0500

    ../.emacs.d/init_lsp_min.el

--- init_lsp_min.el
+++ init_lsp_min.el
@@ -1,4 +1,4 @@
-(ensure-package-installed 'lsp-mode 'dap-mode)
+(ensure-package-installed 'lsp-mode)
 
 ;; c & c++
 (setq c-default-style "linux")
@@ -10,35 +10,20 @@
         (setq c-basic-offset 2)
         (setq indent-tabs-mode t)))
 
+(setq lsp-keymap-prefix "C-'")
 
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
 
 (use-package lsp-mode
-  :commands lsp
   :init
-  (setq lsp-keymap-prefix "C-c z")
-  :config
-  (define-key lsp-mode-map (kbd "C-c z") lsp-command-map)
-  (require 'lsp-clients)
-  :hook (lsp-mode . lsp-enable-which-key-integration))
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-'")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (c++-mode . lsp)
+)
+  :commands lsp)
 
-
-(setq lsp-keymap-prefix "C-'")
-
-;;(use-package lsp-mode
-;;  :init
-;;  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-;;  (setq lsp-keymap-prefix "C-'")
-;;  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-;;         (c++-mode . lsp)
-;;         )
-;;  :commands lsp)
-
-(with-eval-after-load 'lsp-mode
-  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-  (require 'dap-cpptools))
-  ;;(yas-global-mode))
 
 (setq lsp-idle-delay 0.1)  ;; clangd is fast
 
