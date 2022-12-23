dbe1693ecb7696ccc7d8a171e6fcb45f304799e7
Gene Shuman <gene@entropyandsons.com>
2022-12-23 10:28:12 -0500

    ../.emacs.d/init_lsp_min.el

--- init_lsp_min.el
+++ init_lsp_min.el
@@ -19,9 +19,9 @@
 (use-package lsp-mode
   :commands lsp
   :init
-  (setq lsp-keymap-prefix "C-c l")
+  (setq lsp-keymap-prefix "C-g m")
   :config
-  (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
+  (define-key lsp-mode-map (kbd "C-g m") lsp-command-map)
   (require 'lsp-clients)
   :hook (lsp-mode . lsp-enable-which-key-integration))
 
