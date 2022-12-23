c02a7274a9f5b5a848a1ed4aab3bdf1453cca42c
Gene Shuman <gene@entropyandsons.com>
2022-12-23 10:25:42 -0500

    ../.emacs.d/init_lsp_min.el

--- init_lsp_min.el
+++ init_lsp_min.el
@@ -19,7 +19,7 @@
 (use-package lsp-mode
   :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-'")
+  (setq lsp-keymap-prefix "C-;")
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (c++-mode . lsp)
          )
