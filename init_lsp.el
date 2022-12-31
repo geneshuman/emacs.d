2baa62db584e97149969d2693e42a0fab1d5c8fe
Gene Shuman <gene@entropyandsons.com>
2022-12-31 17:31:32 -0500

    init_lsp.el

--- init_lsp.el
+++ init_lsp.el
@@ -85,7 +85,7 @@
   :defer
   :custom
   (dap-auto-configure-mode t "Automatically configure dap.")
-;;  ;;(dap-auto-configure-features locals breakpoints expressions tooltip)  "Remove the button panel in the top.")
+  (dap-auto-configure-features sessions locals breakpoints expressions)  "Remove the button panel in the top.")
   :config
   ;;; dap for c++
   (require 'dap-codelldb))
