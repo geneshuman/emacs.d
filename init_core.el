5c5f30769e9556aaced35dcc9b4ea309a66c265a
Gene Shuman <gene@entropyandsons.com>
2023-01-01 07:41:38 -0500

    init_core.el

--- init_core.el
+++ init_core.el
@@ -219,8 +219,8 @@
 (purpose-mode)
 (define-key purpose-mode-map (kbd "C-x b") nil)
 (define-key purpose-mode-map (kbd "C-x C-f") nil)
-;;(add-to-list 'purpose-user-mode-purposes '(vterm-mode . system))
-;;(purpose-compile-user-configuration)
+(add-to-list 'purpose-user-mode-purposes '(vterm-mode . system))
+(purpose-compile-user-configuration)
 
 ;; (require 'window-purpose-x)
 ;; (purpose-x-magit-single-on)
