f5d647ca4ea9f2254e26997357f3829f2716dc2c
Gene Shuman <gene@entropyandsons.com>
2023-01-01 08:32:08 -0500

    init_complete.el

--- init_complete.el
+++ init_complete.el
@@ -89,7 +89,7 @@ be found in docstring of `posframe-show'."
   "Cleanup posframe if forcus changed"
   (interactive)
   (unless (string-match-p (regexp-quote "*Minibuf") (buffer-name))
-    (message "hi %s" (buffer-name)) ()))
+    (exit-minibuffer) ()))
 
 (setq window-selection-change-functions '(cleanup-posframe))
 
