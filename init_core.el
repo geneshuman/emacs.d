aab366af63d46d75778a13ccc8558a49c853b748
Gene Shuman <gene@entropyandsons.com>
2023-01-01 07:07:50 -0500

    init_core.el

--- init_core.el
+++ init_core.el
@@ -135,7 +135,7 @@
   (if (boundp 'shell-window)
       (select-window shell-window)
     (progn
-      (purpose-toggle-window-purpose-dedicated)
+      ;;(purpose-toggle-window-purpose-dedicated)
       (if (equal major-mode 'vterm-mode)
           ()
         (vterm))
