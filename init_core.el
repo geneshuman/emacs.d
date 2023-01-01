ad1cab8417f8207e834c5a7e1f2c0cdb4389f124
Gene Shuman <gene@entropyandsons.com>
2023-01-01 13:27:05 -0500

    init_core.el

--- init_core.el
+++ init_core.el
@@ -671,7 +671,7 @@
   (multi-vterm)
   (vterm-send-string (concat "ssh entropyandsons@" epi-host))
   (vterm-send-return)
-  (vterm-send-string "tail -f epimorphism/log/log.txt | perl -pe 's/^.*FATAL.*$/\e[1;37;41m$&\e[0m/g; s/^.*ERROR.*$/\e[1;31;40m$&\e[0m/g; s/^.*WARN.*$/\e[0;33;40m$&\e[0m/g; s/^.*INFO.*$/\e[0;36;40m$&\e[0m/g; s/^.*DEBUG.*$/\e[0;37;40m$&\e[0m/g'")
+  (vterm-send-string "tail -f epimorphism/log/log.txt | perl -pe 's/^.*FATAL.*$/\\e[1;37;41m$&\\e[0m/g; s/^.*ERROR.*$/\\e[1;31;40m$&\\e[0m/g; s/^.*WARN.*$/\\e[0;33;40m$&\\e[0m/g; s/^.*INFO.*$/\\e[0;36;40m$&\\e[0m/g; s/^.*DEBUG.*$/\\e[0;37;40m$&\\e[0m/g'")
   (vterm-send-return)
 
   (multi-vterm)
